e7c8164f005ae506e8255d4fcbf3e743b27c52de
PolakinBr <168100151+PolakinBr@users.noreply.github.com>
2024-05-02 16:01:02 -0300

    Add files via upload

---
+++ inicio.js
@@ -0,0 +1,7 @@
+function mostrarCategorias() {
+    document.getElementById('lista-categorias').style.display = 'block';
+}
+
+function esconderCategorias() {
+    document.getElementById('lista-categorias').style.display = 'none';
+}
